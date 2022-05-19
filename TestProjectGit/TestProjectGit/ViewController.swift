da9e1dd1a4bf1320086235b72001667fc47a8494
Никита Ничепорук <tasher228227@gmail.com>
2022-05-19 17:06:14 +0300

    Initial commit

---
+++ TestProjectGit/TestProjectGit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestProjectGit
+//
+//  Created by Никита Ничепорук on 5/19/22.
+//  Copyright © 2022 Никита Ничепорук. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
